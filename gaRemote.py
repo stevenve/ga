c6c9780276789bce15b17a60925ca73948b98fad
SITUBUN <steven.vanessche@gmail.com>
2014-04-22 21:27:15 +0200

    jo

--- gaRemote.py
+++ gaRemote.py
@@ -256,7 +256,7 @@ def executeExperimentParallel(i, j):
     os.chdir('/home/stevenve/argos/argos3/argos3-projects')
     print outputParallel + str(i) + '_' + str(j) + '.argos'
     print os.getcwd()
-    subprocess.Popen(['time','argos3','-c',outputParallel + str(i) + '_' + str(j) + '.argos'], stderr=subprocess.STDOUT, stdout=subprocess.PIPE).communicate()[0]
+    subprocess.Popen(['argos3','-c',outputParallel + str(i) + '_' + str(j) + '.argos'], stderr=subprocess.STDOUT, stdout=subprocess.PIPE).communicate()[0]
     os.chdir('/home/stevenve/gaworkspace/ga')
     print "Experiment finished after " + str(round(time.time()-start,2)) + " seconds."
     
